8c85a578f5fdee8ae3e34ab8d0f3adba2ceed8be
Erlend Walstad <96946613+lampsitter@users.noreply.github.com>
2024-05-09 16:30:06 +0200

    Rename egui_commonmark_macro to plural

--- egui_commonmark_macros/tests/fail/commonmark_str_not_found.rs
+++ egui_commonmark_macros/tests/fail/commonmark_str_not_found.rs
@@ -1,5 +1,5 @@
 use egui::__run_test_ui;
-use egui_commonmark_macro::commonmark_str;
+use egui_commonmark_macros::commonmark_str;
 
 // Check that it fails to compile when it is not able to find a file
 fn main() {
